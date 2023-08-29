592c9ac1b1480f4030f6cef40d04dbbef23a12f3
Hladgerd <hladgerd@gmail.com>
2023-08-29 16:20:13 +0200

    refactor store method in order to use validated data as payload i/o parameters

--- app/Http/Controllers/APIControllers/ContactController.php
+++ app/Http/Controllers/APIControllers/ContactController.php
@@ -3,41 +3,43 @@
 namespace App\Http\Controllers\APIControllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreContactRequest;
 use de\xqueue\maileon\api\client\contacts\Contact;
 use de\xqueue\maileon\api\client\contacts\ContactsService;
 use de\xqueue\maileon\api\client\contacts\Permission;
 use de\xqueue\maileon\api\client\contacts\StandardContactField;
 use de\xqueue\maileon\api\client\contacts\SynchronizationMode;
+use Symfony\Component\HttpFoundation\Response;
 
 class ContactController extends Controller
 {
     /**
      * Create a contact in Maileon
      */
-    public function store(string $email, $fname="", $lname="")
+    public function store(StoreContactRequest $request)
     {
         $contactsService = new ContactsService([
             'API_KEY' => config('services.maileon.key'),
             'DEBUG'=> true // Remove on production config!
         ]);
 
-        $standard_fields = array(
-            StandardContactField::$FIRSTNAME => $fname,
-            StandardContactField::$LASTNAME => $lname,
-        );
+        $standardFields = array();
+        $standardFields[StandardContactField::$FIRSTNAME]=$request->validated('first_name');
+        $standardFields[StandardContactField::$LASTNAME]=$request->validated('last_name');
+
 
         $newContact = new Contact();
-        $newContact->email = $email;
-        $newContact->standard_fields = $standard_fields;
+        $newContact->email = $request->validated('email');
+        $newContact->standard_fields = $standardFields;
         $newContact->permission = Permission::$DOI_PLUS;
 
-        $sync_mode = SynchronizationMode::$UPDATE;
+        $syncMode = SynchronizationMode::$UPDATE;
 
-        $response = $contactsService->createContact($newContact, $sync_mode);
+        $response = $contactsService->createContact($newContact, $syncMode);
 
         return response()
             ->json($response)
-            ->setStatusCode(201);
+            ->setStatusCode(Response::HTTP_CREATED);
     }
 
     /**
