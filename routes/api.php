fbefe23c38009cf5967fea12f158b057dca2b1a8
Hladgerd <hladgerd@gmail.com>
2023-08-30 12:38:30 +0200

    change error message

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\APIControllers\ContactController;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +23,5 @@ Route::post('/create-contact', [ContactController::class, 'store']);
 /**  NON-EXISTENT ROUTES */
 Route::fallback(function(){
     return response()->json([
-        'message' => 'Page Not Found. Try something else'], 404);
+        'message' => 'Incorrect route. Try something else'], Response::HTTP_NOT_FOUND);
 });
